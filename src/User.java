d47a8c51891034701d732221061ab1a378463de6
Loki12 <sergejninzyy@rambler.ru>
2017-01-28 01:55:05 +0300

    Base structure

---
+++ src/User.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ${User} on ${Date}
+ */
+
+class User {
+
+    private String email, login, password;
+    private int height, age;
+    private boolean isMale;
+
+    private List<Car> cars = new ArrayList<>();
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public User(String email, String login, String password) {
+        this.email = email;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean male) {
+        isMale = male;
+    }
+}
