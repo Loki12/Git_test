bd74b9beecef6c761f2dcefb8409dd9b83231f75
Loki12 <sergejninzyy@rambler.ru>
2017-01-28 02:26:07 +0300

    Удалил внутренности

--- src/Car.java
+++ src/Car.java
@@ -5,46 +5,5 @@ import java.awt.*;
  */
 class Car {
 
-    private String mark;
-    private int serisl_number, year;
-    private Color color;
 
-    public Car(String mark, int serisl_number, int year, Color color) {
-        this.mark = mark;
-        this.serisl_number = serisl_number;
-        this.year = year;
-        this.color = color;
-    }
-
-    public String getMark() {
-        return mark;
-    }
-
-    public void setMark(String mark) {
-        this.mark = mark;
-    }
-
-    public int getSerisl_number() {
-        return serisl_number;
-    }
-
-    public void setSerisl_number(int serisl_number) {
-        this.serisl_number = serisl_number;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
 }
